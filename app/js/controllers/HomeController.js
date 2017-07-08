d1c1f437c1b425f2b96c4183960547a3a41f149f
jmcacioppo <jmcacioppo@bellsouth.net>
2017-07-07 22:35:08 -0400

    Added two projects, resume, pictures

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -3,5 +3,10 @@
 josephcacioppo.controller('HomeController',
     function($scope) {
         $scope.title = 'Home Page';
+
+        $(".nav li").on("click", function() {
+            $(".nav li").removeClass("active");
+            $(this).addClass("active");
+        });
     }
 );
