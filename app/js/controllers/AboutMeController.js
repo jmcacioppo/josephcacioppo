e55c6098d88d82953cabb05f4eac1cc0c1524884
jmcacioppo <jmcacioppo@bellsouth.net>
2017-07-11 22:42:37 -0400

    Added some mylife images

--- app/js/controllers/AboutMeController.js
+++ app/js/controllers/AboutMeController.js
@@ -2,22 +2,25 @@
 
 josephcacioppo.controller('AboutMeController',
     function($scope, $http) {
-        $scope.title = 'About Me';
+        //GET SKILLS
         $http.get('../js/services/skills.json')
             .then(function(res) {
                 $scope.skills = res.data;
         });
 
+        //GET MY LIFE EXPERIENCES
         $http.get('../js/services/mylife.json')
             .then(function(res) {
                 $scope.mylife = res.data;
         });
 
+        //GET THE EXPERIENCE THAT IS CLICKED
         $scope.getLifeExp = function(life) {
             $scope.experience = life.id;
             life.isCollapsed = !life.isCollapsed;
         }
 
+        //CHANGE WHICH EXPERIENCE IS COLLAPSED
         $('.row .btn').on('click', function(e) {
             e.preventDefault();
             var $this = $(this);
