82fb2a66116140e544549057fb3618092124ae66
jmcacioppo <jmcacioppo@bellsouth.net>
2017-07-12 23:02:17 -0400

    Collapseable menus working

--- app/js/controllers/AboutMeController.js
+++ app/js/controllers/AboutMeController.js
@@ -13,19 +13,5 @@ josephcacioppo.controller('AboutMeController',
             .then(function(res) {
                 $scope.mylife = res.data;
         });
-
-        //GET THE EXPERIENCE THAT IS CLICKED
-        $scope.getLifeExp = function(life) {
-            $scope.experience = life.id;
-            life.isCollapsed = !life.isCollapsed;
-        }
-
-        //CHANGE WHICH EXPERIENCE IS COLLAPSED
-        $('.row .btn').on('click', function(e) {
-            e.preventDefault();
-            var $this = $(this);
-            var $collapse = $this.closest('.collapse-group').find('.collapse');
-            $collapse.collapse('toggle');
-        });
     }
 );
