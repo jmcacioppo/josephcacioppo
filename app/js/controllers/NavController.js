243c67df06048510c4cf69edd8e001800eb5a649
jmcacioppo <jmcacioppo0531@gmail.com>
2017-12-27 14:04:32 -0500

    Made header fields into an array object

--- app/js/controllers/NavController.js
+++ app/js/controllers/NavController.js
@@ -2,9 +2,35 @@
 
 josephcacioppo.controller('NavController', 
     function($scope, $location) {
+        $scope.navOptions = [{
+            link : "#!/",
+            title : 'Home',
+            active: false
+        }, 
+        {
+            link : "#!/aboutme",
+            title : 'About Me',
+            active: false
+        },
+        {
+            link : "#!/projects",
+            title : 'Projects',
+            active: false
+        },
+        {
+            link : "#!/resume",
+            title : 'Resume',
+            active: false
+        }];
+        
         $scope.isActive = function (viewLocation) {
             var sameLocation = viewLocation === '#!' + $location.path();
             console.log(sameLocation);
             return sameLocation;
+        };
+
+        $scope.checkActive = function (option) {
+            option.active = true;
+            console.log(option);
         };
     });
