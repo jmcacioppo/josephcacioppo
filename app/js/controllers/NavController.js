45e5e57ecc2ef15ea763ed4c698ae3b9c4f4349b
jmcacioppo <jmcacioppo0531@gmail.com>
2017-12-28 17:50:43 -0500

    Changed footer color

--- app/js/controllers/NavController.js
+++ app/js/controllers/NavController.js
@@ -2,26 +2,28 @@
 
 josephcacioppo.controller('NavController', 
     function($scope, $location) {
-        $scope.navOptions = [{
-            link : "#!/",
-            title : 'Home',
-            active: false
-        }, 
-        {
-            link : "#!/aboutme",
-            title : 'About Me',
-            active: false
-        },
-        {
-            link : "#!/projects",
-            title : 'Projects',
-            active: false
-        },
-        {
-            link : "#!/resume",
-            title : 'Resume',
-            active: false
-        }];
+        $scope.navOptions = [
+            {
+                link : "#!/",
+                title : 'Home',
+                active: false
+            }, 
+            {
+                link : "#!/aboutme",
+                title : 'About Me',
+                active: false
+            },
+            {
+                link : "#!/projects",
+                title : 'Projects',
+                active: false
+            },
+            {
+                link : "#!/resume",
+                title : 'Resume',
+                active: false
+            }
+        ];
 
         $scope.checkActive = function (option) {
             if(option.link === '#!' + $location.path()) return true;
