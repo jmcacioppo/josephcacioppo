cca76529a1b8751c22d79b1d938fb06f2dd1839b
jmcacioppo <jmcacioppo0531@gmail.com>
2017-12-31 18:17:50 -0500

    Added actual phone number, removed hr from resume page

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -2,6 +2,8 @@
 
 josephcacioppo.controller('HomeController',
     function($scope) {
+        $scope.phone = "(305) 794-7034";
+
         $(".nav li").on("click", function() {
             $(".nav li").removeClass("active");
             $(this).addClass("active");
