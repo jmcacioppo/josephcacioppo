3dc5906e3aa8e5d05edb863575f1da650e4a9798
jmcacioppo <jmcacioppo0531@gmail.com>
2017-12-30 21:12:57 -0500

    Fixed some wording of sections. Made resume pic clickable

--- app/js/controllers/AboutMeController.js
+++ app/js/controllers/AboutMeController.js
@@ -2,16 +2,23 @@
 
 josephcacioppo.controller('AboutMeController',
     function($scope, $http) {
-        //GET SKILLS
-        $http.get('../js/services/skills.json')
-            .then(function(res) {
-                $scope.skills = res.data;
-        });
+        getSkills();
+        getMyLife();
+        
+        // Get skills
+        async function getSkills() {
+            await $http.get('../js/services/skills.json')
+                .then(function(res) {
+                    $scope.skills = res.data;
+            });
+        }
 
-        //GET MY LIFE EXPERIENCES
-        $http.get('../js/services/mylife.json')
-            .then(function(res) {
-                $scope.mylife = res.data;
-        });
+        // Get life experiences
+        async function getMyLife() {
+            await $http.get('../js/services/mylife.json')
+                .then(function(res) {
+                    $scope.mylife = res.data;
+            });
+        }
     }
 );
