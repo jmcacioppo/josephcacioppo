a33be4565ae8f02a0017e63f84af19b7898ca4e0
jmcacioppo <jmcacioppo0531@gmail.com>
2018-08-28 21:35:28 -0400

    Styling of header

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -2,6 +2,18 @@
 
 josephcacioppo.controller('HomeController',
     function($scope) {
+        let headerBackground = document.getElementById('headerBackground');
+        let homeTitle = document.getElementById('hometitle');
+        headerBackground.style.height = `${window.innerHeight}px`;
+        headerBackground.style.width = `${window.innerWidth}px`;
+        homeTitle.style.marginTop = `${window.innerHeight * .15}px`;
+        
+        window.addEventListener("resize", function (event) {
+            headerBackground.style.height = `${window.innerHeight}px`;
+            headerBackground.style.width = `${window.innerWidth}px`;
+            homeTitle.style.marginTop = `${window.innerHeight * .15}px`;
+        });
+        
         $scope.phone = "(305) 794-7034";
 
         $(".nav li").on("click", function() {
