1192bbb074c53caedc4cf69d37a2a51fa5e209df
jmcacioppo <jmcacioppo0531@gmail.com>
2018-08-28 22:02:25 -0400

    Working on more styling and a debounce function

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -1,18 +1,17 @@
-'use strict';
-
 josephcacioppo.controller('HomeController',
-    function($scope) {
+    function($scope, Util) {
         let headerBackground = document.getElementById('headerBackground');
         let homeTitle = document.getElementById('hometitle');
         headerBackground.style.height = `${window.innerHeight}px`;
         headerBackground.style.width = `${window.innerWidth}px`;
         homeTitle.style.marginTop = `${window.innerHeight * .15}px`;
         
-        window.addEventListener("resize", function (event) {
+        window.addEventListener("resize", Util.debounce(function (event) {
             headerBackground.style.height = `${window.innerHeight}px`;
             headerBackground.style.width = `${window.innerWidth}px`;
             homeTitle.style.marginTop = `${window.innerHeight * .15}px`;
-        });
+            console.log('yo');
+        }), 10000);
         
         $scope.phone = "(305) 794-7034";
 
---
+++ app/js/utilities/debounce.js
@@ -0,0 +1,21 @@
+josephcacioppo.factory('Util', function () {
+    return {
+        debounce: function (func, wait, immediate) {
+            var timeout;
+
+            return function () {
+                var context = this,
+                    args = arguments;
+                var callNow = immediate && !timeout;
+
+                clearTimeout(timeout);
+                timeout = setTimeout(function () {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                }, wait);
+
+                if (callNow) func.apply(context, args);
+            };
+        }
+    }
+});
