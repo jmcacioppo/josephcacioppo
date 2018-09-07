d30a158ff6f074cb0306a9fae78d1f018bb7605b
jmcacioppo <jmcacioppo0531@gmail.com>
2018-09-06 21:35:03 -0400

    Working on projects styling

--- app/js/controllers/ProjectsController.js
+++ app/js/controllers/ProjectsController.js
@@ -1,13 +1,19 @@
-josephcacioppo.controller('ProjectsController', function($scope, $http) {
-  $scope.title = 'Projects Page';
+josephcacioppo.controller('ProjectsController', function($scope, $http, $timeout) {
+  
   $http.get('../js/services/projects.json')
     .then((response) => $scope.projects = response.data);
 
   $scope.mouseIsOver = function(project) {
-    project.showData = true;
+    project.hideImage = true;
+
+    $timeout(function () {
+      project.showData = true;
+    }, 550);
   };
   
   $scope.mouseIsGone = function(project) {
     project.showData = false;
+    project.hideImage = false;
   };
+  
 });
