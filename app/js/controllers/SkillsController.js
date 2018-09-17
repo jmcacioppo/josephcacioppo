7c40187d9a1852d254125155b746fec94bacc423
jmcacioppo <jmcacioppo0531@gmail.com>
2018-09-16 20:15:27 -0400

    Added caching to http requests, got rid of unused code

--- app/js/controllers/SkillsController.js
+++ app/js/controllers/SkillsController.js
@@ -2,7 +2,7 @@ josephcacioppo.controller('SkillsController', function($scope, $http) {
   getSkills();
   
   async function getSkills() {
-    await $http.get('../js/services/skills.json')
+    await $http.get('../js/services/skills.json', { cache: true })
       .then(response => $scope.skills = response.data);
   }
 });
