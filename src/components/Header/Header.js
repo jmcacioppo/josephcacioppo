b1a7eb318291b4bd4e936c526d3c1569ca057952
Joseph Cacioppo <jmcacioppo0531@gmail.com>
2019-08-05 22:02:56 -0400

    Styling header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,40 +2,42 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => (
-  <header className="Header">
-    <img
-      className="JCLogo"
-      src="/img/logo/SiteLogo.png"
-      alt="Joseph Cacioppo - Software Developer"
-    />
-
-    <a
-      href="https://www.linkedin.com/in/joseph-cacioppo-a05355135/"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
+  <header className="Header FlexContainer FlexColumn">
+    <div className="FlexContainer JustifyContentSpaceBetween">
       <img
-        className="SocialLogo"
-        src="/img/logo/LinkedInLogo.png"
-        alt="Joseph Cacioppo - LinkedIn"
+        className="JCLogo"
+        src="/img/logo/SiteLogo.png"
+        alt="Joseph Cacioppo - Software Developer"
       />
-    </a>
 
-    <a
-      href="https://github.com/jmcacioppo"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <img
-        className="SocialLogo"
-        src="/img/logo/GitHubLogo.png"
-        alt="Joseph Cacioppo - Github"
-      />
-    </a>
+      <div>
+        <a
+          href="https://github.com/jmcacioppo"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className="SocialLogo"
+            src="/img/logo/GitHubLogo.png"
+            alt="Joseph Cacioppo - Github"
+          />
+        </a>
 
-    <span className="VerticalSpacing" />
+        <a
+          href="https://www.linkedin.com/in/joseph-cacioppo-a05355135/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className="SocialLogo"
+            src="/img/logo/LinkedInLogo.png"
+            alt="Joseph Cacioppo - LinkedIn"
+          />
+        </a>
+      </div>
+    </div>
 
-    <div className="CenterText">
+    <div className="HeaderTextContainer CenterText FlexContainer FlexColumn JustifyContentCenter">
       <h1 className="HeaderTitle">
         <b>Joseph Cacioppo</b>
       </h1>
