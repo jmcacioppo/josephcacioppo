f4ef0c1d1356ab5e73896a949ee754b34929e499
Joseph Cacioppo <jmcacioppo0531@gmail.com>
2019-09-30 22:08:27 -0400

    More styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,10 +9,16 @@ const ContactPage = () => (
     <h1 className="HeaderTitle">Contact</h1>
     <div className="FlexContainer AlignItemsCenter " style={{ height: "75%" }}>
       <p className="Paragraph CenterText">
-        If you&#39;d like to speak, feel free to contact me at{" "}
-        <a style={{ color: "#f8f8f8" }} href="mailto:jmcacioppo0531@gmail.com">
-          jmcacioppo0531@gmail.com
-        </a>
+        If you&#39;d like to chat, feel free to contact me here:
+        <br />
+        <span>
+          <a
+            style={{ color: "#f8f8f8" }}
+            href="mailto:jmcacioppo0531@gmail.com"
+          >
+            jmcacioppo0531@gmail.com
+          </a>
+        </span>
       </p>
     </div>
   </Layout>
